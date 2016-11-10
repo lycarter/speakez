bdedd9a1e7f0c265b34d3a5fdca9a94f49efbfe4
David Wong <dhyhwong@gmail.com>
2016-11-09 21:56:38 -0500

    add serial printouts for the data input pin

--- arduino/analog_clock/analog_clock.ino
+++ arduino/analog_clock/analog_clock.ino
@@ -1,5 +1,6 @@
 int clk = 3;
 int led = 13;
+int data = 16;
 
 void setup() {
   // sanity check
@@ -10,8 +11,15 @@ void setup() {
   pinMode(clk, OUTPUT);
   analogWriteResolution(1);
   analogWriteFrequency(clk, 3000000);
-  analogWrite(clk, 1); // set duty cycle to 50%
+  analogWrite(clk, 1); // set duty cycle to 50%, (half of 2^resolution)
+
+  // prepare data pin
+  pinMode(data, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  int dataValue = digitalRead(data);
+  Serial.println(dataValue);
+  delayMicroseconds(1);
 }
